0ef9d99eea00d839c2e679ba174e0bc791556938
Tarashdeep Singh <92468703+tarashdeepsingh@users.noreply.github.com>
2024-11-06 10:36:26 +0530

    Add files via upload

---
+++ src/Observers/UserObserver.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Avalon\LrvLogin\Src\Observers;
+
+use Avalon\LrvLogin\Src\Services\EmailService;
+use App\Models\User;
+
+class UserObserver
+{
+    protected $emailService;
+
+    public function __construct(EmailService $emailService)
+    {
+        $this->emailService = $emailService;
+    }
+
+    public function created(User $user)
+    {
+        $this->emailService->sendEmail([
+            'email_from' => env('MAIL_FROM_ADDRESS', 'no-reply@example.org'),
+            'email_to' => $user->email,
+            'email_subject' => $userData['subject'],
+            'email_content' => $userData['content']
+        ]);
+    }
+}
---
+++ src/Services/EmailService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Avalon\LrvLogin\Src\Services;
+use Illuminate\Support\Facades\Http;
+
+class EmailService
+{
+    protected $emailServiceUrl;
+
+    public function __construct()
+    {
+        $this->emailServiceUrl = config('custom_login.email_service_url', 'https://service-discovery.dataforall.org/v1/catalog/service/dfa_mailer');
+    }
+
+    public function sendEmail($emailTo, $emailFrom, $subject, $content)
+    {
+        return Http::post("{$this->emailServiceUrl}/send-email", [
+            'email_to' => $emailTo,
+            'email_from' => $emailFrom,
+            'email_subject' => $subject,
+            'email_content' => $content,
+        ])->successful();
+    }
+}
